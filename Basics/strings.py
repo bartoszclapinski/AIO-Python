012aebe035ada269e4f57509aafeb896e4324bd2
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-02-03 14:45:57 +0100

    Basics\strings - f-strings

--- Basics/strings.py
+++ Basics/strings.py
@@ -16,3 +16,19 @@ print(greeting + name)
 # We can also do this:
 name = input("Please enter your name ")
 print(greeting + ' ' + name)
+
+age_in_words = "2 years"
+print(name + " is " + age_in_words + " years old")
+age = 24
+print(name + " is " + age + " years old") # This will throw an error
+
+# To fix the above error, we can do this:
+print(name + " is " + str(age) + " years old")
+print(type(age))
+print(name + f" is {age} years old")
+print(type(age))
+
+print('#' * 40)
+print(f"Pi is approximately {22 / 7:12.50f}")
+pi = 22 / 7
+print(f"Pi is approximately {pi:12.50f}")
