56bfd3ac1a2a2655851286b2661e1a09f4ca764f
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-02-04 12:06:07 +0100

    Basics\interpolation

---
+++ Basics/interpolation.py
@@ -0,0 +1,9 @@
+age = 24
+print("My age is %d years" % age)
+
+major = "years"
+minor = "months"
+print("My age is %d %s, %d %s" % (age, major, 6, minor))   # This is called tuple interpolation
+print("Pi is approximately %f" % (22 / 7))  # This is called float interpolation
+print("Pi is approximately %60.50f" % (22 / 7))  # This is called float interpolation with width and precision
+
