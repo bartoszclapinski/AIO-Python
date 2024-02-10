b8aef26ccc90fd6ff25618396f295a9b7c3d59ee
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-02-10 23:41:59 +0100

    FlowControl\aa - augmented assignment

---
+++ FlowControl/aa.py
@@ -0,0 +1,25 @@
+# Augmented Assignment
+# +=, -=, *=, /=, %=, **=, //=
+
+a = 10
+
+a += 5
+print(a)
+
+a -= 5
+print(a)
+
+a *= 5
+print(a)
+
+a /= 5
+print(a)
+
+a %= 5
+print(a)
+
+a **= 5
+print(a)
+
+a //= 5
+print(a)
