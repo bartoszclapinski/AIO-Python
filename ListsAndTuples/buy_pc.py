ed5434315af3b43022a5352713596b9ee15c957b
Bartosz Cłapiński <bartosz.clapinski@gmail.com>
2024-05-02 22:55:18 +0200

    Removing items from the list

--- ListsAndTuples/buy_pc.py
+++ ListsAndTuples/buy_pc.py
@@ -1,15 +1,20 @@
 current_choice = "-"
 available_parts = ["computer", "monitor", "keyboard", "mouse", "mouse mat", "hdmi cable"]
-computer_parts = [] # create an empty list
+computer_parts = []
 valid_choices = [str(i) for i in range(1, len(available_parts) + 1)]
 
 while current_choice != '0':
     if current_choice in valid_choices:
-        print("Adding {}".format(current_choice))
         index = int(current_choice) - 1
         chosen_part = available_parts[index]
-        computer_parts.append(chosen_part)
-        
+        if chosen_part in computer_parts:
+            print("Removing {}".format(current_choice))
+            computer_parts.remove(chosen_part)
+        else:
+            print("Adding {}".format(current_choice))
+            computer_parts.append(chosen_part)
+        print("Your list now contains: {}".format(computer_parts))
+
     else:
         print("Please add options from the list below:")
         for number, part in enumerate(available_parts):
