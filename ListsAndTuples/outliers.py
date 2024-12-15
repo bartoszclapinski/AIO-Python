abe1f6c017db9ba20f2c461c9965a032dff0a732
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-12-15 13:39:42 +0100

    safely removing items from list

--- ListsAndTuples/outliers.py
+++ ListsAndTuples/outliers.py
@@ -11,9 +11,30 @@ min_valid = 100
 max_valid = 200
 
 data = [4, 5, 104, 105, 110, 120, 130, 150, 160, 170, 183, 185, 187, 188, 191, 350, 360]
-
 for index, value in enumerate(data):
     if (value < min_valid) or (value > max_valid):        
-        del data[index]            
+        del data[index]
+print(data)
 
+data = [4, 5, 104, 105, 110, 120, 130, 150, 160, 170, 183, 185, 187, 188, 191, 350, 360]
+stop = 0
+for index, value in enumerate(data):
+    if value >= min_valid:
+        stop = index
+        break;
+
+print(stop)
+del data[:stop]
+print(data)
+
+data = [4, 5, 104, 105, 110, 120, 130, 150, 160, 170, 183, 185, 187, 188, 191, 350, 360]
+
+start = 0
+for index in range(len(data) - 1, -1, -1):
+    if data[index] <= max_valid:
+        start = index + 1
+        break;
+
+print(start)
+del data[:start]
 print(data)
