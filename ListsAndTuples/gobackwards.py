f8157dd5efefbe07e7128ff262fe76d79e9b904a
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-12-17 11:56:15 +0100

    reverse function

--- ListsAndTuples/gobackwards.py
+++ ListsAndTuples/gobackwards.py
@@ -9,3 +9,13 @@ for index in range(len(data) - 1, -1, -1):
         del data[index]
 
 print(data)
+
+data = [104, 101, 4, 105, 308, 103, 5, 107, 100, 306, 106, 102, 108]
+
+top_index = len(data) - 1
+for index, value in enumerate(reversed(data)):
+    if value < min_valid or value > max_valid:  
+        print(top_index - index, data)
+        del data[top_index - index]
+
+print(data)
