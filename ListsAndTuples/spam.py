0ac0c366515e8a3ddffb33548358b153dccf212c
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-12-19 17:16:34 +0100

    no spam challenge

--- ListsAndTuples/spam.py
+++ ListsAndTuples/spam.py
@@ -9,3 +9,12 @@ menu = [
     ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
 ]
  
+for meal in menu:
+    if "spam" not in meal:
+        print(meal)
+
+        for item in meal:
+            print(item)
+    else:
+        print("{0} has a spam score of {1}".format(meal, meal.count("spam")))
+    
