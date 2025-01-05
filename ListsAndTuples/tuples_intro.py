fde4607da50863896e41b0374b70906f1073abdb
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2025-01-05 14:11:58 +0100

    tuples are immutable

--- ListsAndTuples/tuples_intro.py
+++ ListsAndTuples/tuples_intro.py
@@ -9,5 +9,22 @@ age = 28
 print(name, age, "Python", 2024)
 print((name, age, "Python", 2024))
 
+welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
+bad = "Bad Company", "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Emilda May", 2011
+metallica = "Ride the Lightning", "Metallica", 1984
 
+print(metallica)
+print(metallica[0])
+print(metallica[1])
+print(metallica[2])
 
+# metallica[0] = "Master of Puppets"
+# We cannot assign to a tuple, tuples are immutable
+
+metallica2 = list(metallica)
+print(metallica2)
+
+metallica2[0] = "Master of Puppets"
+print(metallica2)
