9b30d6e381eab0f6ea362e0e3b29cd35d6a1de6c
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2025-01-06 16:39:09 +0100

    unpacking tuples

---
+++ ListsAndTuples/unpacking.py
@@ -0,0 +1,25 @@
+a = b = c = d = e = f = 12
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
+x, y = 1, 2
+print(x)
+print(y)
+
+print("Unpacking a tuple")
+data = 1, 2, 76
+x, y, z = data
+print(x)
+print(y)
+print(z)
+
+print("Unpacking a list")
+data_list = [12, 13, 14]
+p, q, r = data_list
+print(p)
+print(q)
+print(r)
