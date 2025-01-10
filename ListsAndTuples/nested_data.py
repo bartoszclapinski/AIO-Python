fb0e73278c26a0d6ae6f1aa55b4dee78642f1b43
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2025-01-10 12:40:42 +0100

    nested data structures

---
+++ ListsAndTuples/nested_data.py
@@ -0,0 +1,73 @@
+albums = [
+    ("Welcome to my Nightmare", "Alice Cooper", 1975,
+     [
+        (1, "Welcome to my Nightmare"),
+        (2, "Devil's Food"),
+        (3, "The Black Widow"),
+        (4, "Some Folks"),
+        (5, "Only Women Bleed")
+     ]
+    ),
+    ("Bad Company", "Bad Company", 1974,
+     [
+        (1, "Can't Get Enough"),
+        (2, "Rock Steady"),
+        (3, "Ready for Love"),
+        (4, "Don't Let Me Down"),
+        (5, "Bad Company"),
+        (6, "The Way I Choose"),
+        (7, "Movin' On"),
+        (8, "Seagull")
+     ]
+    ),
+    ("Nightflight", "Budgie", 1981,
+     [
+        (1, "I Turned to Stone"),
+        (2, "Keeping a Rendezvous"),
+        (3, "Reaper of the Glory"),
+        (4, "She Used Me Up")
+     ]
+    ),
+    ("More Mayhem", "Emilda May", 2011,
+     [
+        (1, "Pulling the Rug"),
+        (2, "Psycho"),
+        (3, "Mayhem"),
+        (4, "Kentish Town Waltz")
+     ]
+    )
+]
+
+for name, artist, year, songs in albums:
+    print("Album: {}, Artist: {}, Year: {}, Songs: {}".format(name, artist, year, songs))
+
+print("=" * 50)
+
+album = albums[2]
+print(album)
+
+print("=" * 50)
+
+songs = album[3]
+print(songs)
+
+print("=" * 50)
+
+song = songs[3]
+print(song)
+
+print("=" * 50)
+
+title, track = song
+print(title)
+print(track)
+
+print("=" * 50)
+
+title, artist, year, songs = album
+print(title)
+print(artist)
+print(year)
+print(songs)
+
+
