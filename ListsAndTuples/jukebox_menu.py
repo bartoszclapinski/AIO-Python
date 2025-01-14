314d288c00b49007bcb21c2c932ebbd0185b4bf3
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2025-01-14 22:13:22 +0100

    constants

--- ListsAndTuples/jukebox_menu.py
+++ ListsAndTuples/jukebox_menu.py
@@ -1,8 +1,21 @@
 from albums_data import albums
 
+SONGS_LIST_INDEX = 3
+
 while True:
     print("Please choose your album (invalid choice exits):")
     for index, (title, artist, year, songs) in enumerate(albums):
-        print(f"{index + 1}: {title} by {artist} ({year}), songs: {songs}")
-    break
+        print(f"{index + 1}: {title}")
+    
+    album_choice = int(input())
+    if 1 <= album_choice <= len(albums):
+        songs_list = albums[album_choice - 1][SONGS_LIST_INDEX]        
+    else:
+        break
+
+    print(albums[album_choice - 1])
+    print(songs_list)
+
+
+    
 
