a0f13f90c00c64e8b2425d26442b6fff36f01f10
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2025-01-20 23:14:23 +0100

    palindromes

--- Functions/functions.py
+++ Functions/functions.py
@@ -2,6 +2,11 @@ def multiply(x, y):
     result = x * y
     return result
 
+
+def is_palindrome(word):
+    backwards = word[::-1].casefold()
+    return backwards == word.casefold()
+
 answer = multiply(10.5, 4)
 print(answer)
 
@@ -13,3 +18,9 @@ print()
 for val in range(1, 5):
     two_times = multiply(2, val)
     print(two_times)
+
+word = input("Enter a word: ")
+if is_palindrome(word):
+    print(f"{word} is a palindrome")
+else:
+    print(f"{word} is not a palindrome")
