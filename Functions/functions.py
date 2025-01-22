1da0afb00eff902c87b57bd91062a9f86ddf0d6a
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2025-01-22 23:24:29 +0100

    sentence palindrome

--- Functions/functions.py
+++ Functions/functions.py
@@ -7,6 +7,15 @@ def is_palindrome(word):
     backwards = word[::-1].casefold()
     return backwards == word.casefold()
 
+
+def palindrome_sentence(sentence):
+    string = ""
+    for char in sentence:
+        if char.isalnum():
+            string += char
+    return is_palindrome(string)
+
+
 answer = multiply(10.5, 4)
 print(answer)
 
@@ -24,3 +33,10 @@ if is_palindrome(word):
     print(f"{word} is a palindrome")
 else:
     print(f"{word} is not a palindrome")
+
+sentence = input("Enter a sentence: ")
+if palindrome_sentence(sentence):
+    print(f"{sentence} is a palindrome")
+else:
+    print(f"{sentence} is not a palindrome")
+
