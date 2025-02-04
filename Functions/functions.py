d6258f3531434c7ccd26af43ca2f4705ccae404e
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2025-02-04 20:32:28 +0100

    docstrings even more

--- Functions/functions.py
+++ Functions/functions.py
@@ -1,14 +1,42 @@
 def multiply(x, y):
-    result = x * y
+    """
+    Multiplies two numbers
+
+    Although this function is intended to multiply 2 numbers,
+    you can also use it to multiply a sequence.  If you pass
+    a string, for example, as the first argument, you'll get
+    the string repeated `y` times as the returned value.
+ 
+    :param x: The first number to multiply.
+    :param y: The number to multiply `x` by.
+    :return: The product of `x` and `y`.
+    """
+    return x * y
     #return result
 
 
 def is_palindrome(word):
+    """
+    Check if a word is a palindrome.
+
+    A palindrome is a word that reads the same forwards and backwards.
+
+    :param word: The word to check.
+    :return: True if the word is a palindrome, False otherwise.
+    """
     backwards = word[::-1].casefold()
     return backwards == word.casefold()
 
 
 def palindrome_sentence(sentence):
+    """
+    Check if a sentence is a palindrome.
+
+    A palindrome is a sentence that reads the same forwards and backwards.
+
+    :param sentence: The sentence to check.
+    :return: True if the sentence is a palindrome, False otherwise.
+    """
     string = ""
     for char in sentence:
         if char.isalnum():
@@ -17,7 +45,13 @@ def palindrome_sentence(sentence):
 
 
 def sum_eo(n, t):
-    
+    """
+    Sum even or odd numbers in a given range.
+
+    :param n: The end of the range (inclusive).
+    :param t: 'e' to sum even numbers, 'o' to sum odd numbers.
+    :return: The sum of even or odd numbers in the range.
+    """
     if t not in ['e', 'o']:
         return -1
         
