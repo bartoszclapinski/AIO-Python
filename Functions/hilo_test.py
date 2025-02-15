4c4c4a76f2aa25a32b86c4adb1ddeb30d0a728be
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2025-02-15 23:07:39 +0100

    counting correct guesses

--- Functions/hilo_test.py
+++ Functions/hilo_test.py
@@ -19,8 +19,18 @@ def guess_binary(answer, low, high):
         guesses += 1
 
 
+correct_count = 0   
+max_guesses = 0
+
 for number in range(LOW, HIGH + 1):
     number_of_guesses = guess_binary(number, LOW, HIGH)
     print("{} guessed in {}".format(number, number_of_guesses))
+    
+    if number_of_guesses > max_guesses:
+        max_guesses, correct_count = number_of_guesses, 1
+    elif number_of_guesses == max_guesses:
+        correct_count += 1
 
+print("I guessed without being told {} times".format(correct_count))
+print("The maximum number of guesses was {}".format(max_guesses))
 
