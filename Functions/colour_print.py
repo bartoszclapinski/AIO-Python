c0dcc3986cc8077b0c34a1fdced046b67c094950
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2025-03-11 17:58:32 +0100

    enhancing the colour_print with multiple ansi escape sequences

--- Functions/colour_print.py
+++ Functions/colour_print.py
@@ -16,29 +16,21 @@ UNDERLINE = '\u001b[4m'
 REVERSE = '\u001b[7m'
 
 
-def colour_print(text: str, effect: str) -> None:
+def colour_print(text: str, *effects: str) -> None:
     """
     Print `text` using the ANSI sequence to change colour, etc.
 
     :param text: The text to print.
-    :param effect: The effect to apply to the text.    
+    :param effects: The effects to apply to the text.
     """
-    output_string = "{0}{1}{2}".format(effect, text, RESET)
+    effect_string = "".join(effects)
+    output_string = "{0}{1}{2}".format(effect_string, text, RESET)
     print(output_string)
 
 
-print(RED, "this will be in red")
-print("and so is this")
-print(GREEN, "this will be in green")
-print(BLUE, "this will be in blue")
-print(BOLD, "this will be in bold")
-print(UNDERLINE, "this will be underlined")
-print(REVERSE, "this will be reversed")
-print(RESET, "this will be reset")
-
 colorama.init()
-colour_print("Hello in red", RED)
-colour_print("Hello in green", GREEN)
+colour_print("Hello in red", BOLD, RED, UNDERLINE)
+colour_print("Hello in green", GREEN, UNDERLINE, REVERSE)
 colour_print("Hello in blue", BLUE)
 colour_print("Hello in bold", BOLD)
 colour_print("Hello in underline", UNDERLINE)
